2da32281db5581073a1d4dddfeb4ee9f4f5fdc0a
wh1tly337 <102896351+wh1tly337@users.noreply.github.com>
2022-12-05 12:28:56 +0500

    Delete app directory

--- app/ekadom/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import main_page, ads_page, about_page, contacts_page
-
-urlpatterns = [
-    path('', main_page),
-    path('ads', ads_page),
-    path('about', about_page),
-    path('contacts', contacts_page)
-]
