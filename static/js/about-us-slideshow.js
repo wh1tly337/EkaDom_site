db7b87d97a547f5e5133d6eb09c8060773679cfe
wh1tly337 <wh1tly337@gmail.com>
2023-01-17 15:33:07 +0500

    beauty commit

--- static/js/about-us-slideshow.js
+++ static/js/about-us-slideshow.js
@@ -1,6 +1,6 @@
 var controls = document.querySelectorAll('.about-us-controls');
 for (var i = 0; i < controls.length; i++) {
-  controls[i].style.display = 'inline-block';
+    controls[i].style.display = 'inline-block';
 }
 
 var slides = document.querySelectorAll('#about-us-slides .about-us-slide');
@@ -8,17 +8,17 @@ var currentSlide = 0;
 var slideInterval = setInterval(nextSlide, 5000);
 
 function nextSlide() {
-  goToSlide(currentSlide + 1);
+    goToSlide(currentSlide + 1);
 }
 
 function previousSlide() {
-  goToSlide(currentSlide - 1);
+    goToSlide(currentSlide - 1);
 }
 
 function goToSlide(n) {
-  slides[currentSlide].className = 'about-us-slide';
-  currentSlide = (n + slides.length) % slides.length;
-  slides[currentSlide].className = 'about-us-slide about-us-showing';
+    slides[currentSlide].className = 'about-us-slide';
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].className = 'about-us-slide about-us-showing';
 }
 
 
@@ -26,30 +26,33 @@ var playing = true;
 var pauseButton = document.getElementById('about-us-pause');
 
 function pauseSlideshow() {
-  pauseButton.innerHTML = '&#9658;'; // play character
-  playing = false;
-  clearInterval(slideInterval);
+    pauseButton.innerHTML = '&#9658;'; // play character
+    playing = false;
+    clearInterval(slideInterval);
 }
 
 function playSlideshow() {
-  pauseButton.innerHTML = '&#10074;&#10074;'; // pause character
-  playing = true;
-  slideInterval = setInterval(nextSlide, 2000);
+    pauseButton.innerHTML = '&#10074;&#10074;'; // pause character
+    playing = true;
+    slideInterval = setInterval(nextSlide, 2000);
 }
 
 pauseButton.onclick = function () {
-  if (playing) { pauseSlideshow(); }
-  else { playSlideshow(); }
+    if (playing) {
+        pauseSlideshow();
+    } else {
+        playSlideshow();
+    }
 };
 
 var next = document.getElementById('about-us-next');
 var previous = document.getElementById('about-us-previous');
 
 next.onclick = function () {
-  pauseSlideshow();
-  nextSlide();
+    pauseSlideshow();
+    nextSlide();
 };
 previous.onclick = function () {
-  pauseSlideshow();
-  previousSlide();
+    pauseSlideshow();
+    previousSlide();
 };
--- static/js/partners-slideshow.js
+++ static/js/partners-slideshow.js
@@ -1,6 +1,6 @@
 var partners_controls = document.querySelectorAll('.partners-controls');
 for (var i = 0; i < partners_controls.length; i++) {
-  partners_controls[i].style.display = 'inline-block';
+    partners_controls[i].style.display = 'inline-block';
 }
 
 var partners_slides = document.querySelectorAll('#partners-slides .partners-slide');
@@ -8,25 +8,25 @@ var partners_currentSlide = 0;
 var partners_slideInterval = setInterval(partners_nextSlide, 10000);
 
 function partners_nextSlide() {
-  partners_goToSlide(partners_currentSlide + 1);
+    partners_goToSlide(partners_currentSlide + 1);
 }
 
 function partners_previousSlide() {
-  partners_goToSlide(partners_currentSlide - 1);
+    partners_goToSlide(partners_currentSlide - 1);
 }
 
 function partners_goToSlide(n) {
-  partners_slides[partners_currentSlide].className = 'partners-slide';
-  partners_currentSlide = (n + partners_slides.length) % partners_slides.length;
-  partners_slides[partners_currentSlide].className = 'partners-slide partners-showing';
+    partners_slides[partners_currentSlide].className = 'partners-slide';
+    partners_currentSlide = (n + partners_slides.length) % partners_slides.length;
+    partners_slides[partners_currentSlide].className = 'partners-slide partners-showing';
 }
 
 var partners_next = document.getElementById('partners-next');
 var partners_previous = document.getElementById('partners-previous');
 
 partners_next.onclick = function () {
-  partners_nextSlide();
+    partners_nextSlide();
 };
 partners_previous.onclick = function () {
-  partners_previousSlide();
+    partners_previousSlide();
 };
